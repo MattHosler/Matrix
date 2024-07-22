8dc12c1bca740c5d4b203e0d8dfbaa0cdcd0d4e4
Matt Hosler <61326348+MattHosler@users.noreply.github.com>
2024-07-22 12:26:42 -0400

    Update main.cpp

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int matrix[5][5]{
+        {1,2,3,4,5},
+        {6,7,8,9,10},
+        {11,12,13,14,15},
+        {16,17,18,19,20},
+        {21,22,23,24,25}
+};
+int rowSize = 5;
+int columnSize = 5;
+int maxRow = 0;
+
+
+
+int main() {
+    int total= 0;
+    for (int column =0; column < columnSize; column++) {
+        for (int row = 0; row < rowSize; row++) {
+
+            total += matrix[row][column];
+            if (matrix[row][column]> maxRow){
+                maxRow= matrix[row][column];
+            }
+        }
+        cout << "Sum for column" << column << "is" << total << endl;
+
+    }
+    cout << "Max of all is: "<< maxRow<< endl;
+
+
+}
